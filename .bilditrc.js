237cf0d8b1acb4dbc2480fd8c22e22ff82e37aa3
Gil Tayar <gil@tayar.org>
2017-08-26 16:11:53 +0300

    added git information to bilditrc

--- .bilditrc.js
+++ .bilditrc.js
@@ -18,7 +18,9 @@ module.exports = {
     "builder:npm": "@bildit/npm-build-job",
     "publisher:npm": {"@bildit/npm-publisher-with-git": {
       npmAuthenticationLine: "//registry.npmjs.org/:_authToken=faf41d9d-7071-4a7a-9369-d5a85d1017c0",
-      gitAuthenticationKey: fs.readFileSync(path.join(process.env.HOME, '.ssh/id_rsa_github'))
+      gitAuthenticationKey: fs.readFileSync(path.join(process.env.HOME, '.ssh/id_rsa_github')),
+      gitUserEmail: 'gil@tayar.org',
+      gitUserName: 'Gil Tayar'
     }}
   },
   publish: true
