2eb93ed6f891c69b174bcd087d363839a085be08
Gil Tayar <gil@tayar.org>
2017-08-26 16:13:37 +0300

    added npm access to bilditrc

--- .bilditrc.js
+++ .bilditrc.js
@@ -20,7 +20,8 @@ module.exports = {
       npmAuthenticationLine: "//registry.npmjs.org/:_authToken=faf41d9d-7071-4a7a-9369-d5a85d1017c0",
       gitAuthenticationKey: fs.readFileSync(path.join(process.env.HOME, '.ssh/id_rsa_github')),
       gitUserEmail: 'gil@tayar.org',
-      gitUserName: 'Gil Tayar'
+      gitUserName: 'Gil Tayar',
+      access: 'public'
     }}
   },
   publish: true
