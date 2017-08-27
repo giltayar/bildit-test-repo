f406b17a0e266f9e3132fd8deea7e25011e53423
Gil Tayar <gil@tayar.org>
2017-08-27 11:19:57 +0300

    returned npm/git auth

--- .bilditrc.js
+++ .bilditrc.js
@@ -17,7 +17,12 @@ module.exports = {
     },
     "builder:npm": "@bildit/npm-build-job",
     "publisher:npm": {"@bildit/npm-publisher-with-git": {
-      access: 'public'
+      access: 'public',
+      npmAuthenticationLine: fs.readFileSync(path.join(process.env.HOME, '.npmrc'), 'utf-8').split('\n')
+        .find(l => l.includes('authToken')),
+      gitAuthenticationKey: fs.readFileSync(path.join(process.env.HOME, '.ssh/id_rsa_github')),
+      gitUserEmail: 'gil@tayar.org',
+      gitUserName: 'Gil Tayar',
     }}
   },
   publish: true
